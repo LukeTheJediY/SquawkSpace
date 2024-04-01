67b73093039a4b5cc2d3e62f4e505320f0d79def
LukeTheJediY <165628934+LukeTheJediY@users.noreply.github.com>
2024-04-01 01:13:39 -0400

    Update Clients.js

--- Clients.js
+++ Clients.js
@@ -2,9 +2,14 @@ const socket = new WebSocket('https://squawkspace.netlify.app/');
 
 socket.onmessage = function(event) {
     const message = JSON.parse(event.data);
-    // Display the received message in the UI
+    // Display the received message in the UI for all users
 };
 
 function sendMessage(message) {
+    // Display the sent message locally in the UI
+    // (optional, to provide immediate feedback to the sender)
+    // const localMessage = ...;
+
+    // Send the message to the server
     socket.send(JSON.stringify({ message }));
 }
