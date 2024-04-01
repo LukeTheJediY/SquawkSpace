fe882544ed34391ae1f97474f96346db59b23519
LukeTheJediY <165628934+LukeTheJediY@users.noreply.github.com>
2024-04-01 01:10:42 -0400

    Update Clients.js

--- Clients.js
+++ Clients.js
@@ -1,13 +1,10 @@
-// Connect to the WebSocket server
-const socket = new WebSocket("https://squawkspace.netlify.app/");
+const socket = new WebSocket('https://squawkspace.netlify.app/');
 
-// Handle incoming messages from the server
 socket.onmessage = function(event) {
-    console.log("Message from server:", event.data);
-    // Update UI or perform actions based on received messages
+    const message = JSON.parse(event.data);
+    // Display the received message in the UI
 };
 
-// Send messages to the server
 function sendMessage(message) {
-    socket.send(message);
+    socket.send(JSON.stringify({ message }));
 }
